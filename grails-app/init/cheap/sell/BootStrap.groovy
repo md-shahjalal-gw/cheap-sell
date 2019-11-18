d7eb4c34bd39c2bbe8f48b8275d78da726d402ff
Md Shahjalal <shahjalal@gwu.edu>
2019-11-17 21:29:26 -0500

    Update code for item

--- grails-app/init/cheap/sell/BootStrap.groovy
+++ grails-app/init/cheap/sell/BootStrap.groovy
@@ -15,19 +15,19 @@ class BootStrap {
         }
 
         if (!Login.findByUsername('jyoung')) {
-            def l = new Login(username: 'jyoung', password: 'gwu123', email: 'anc@email.com')
+            def l = new Login(username: 'jyoung', password: 'gwu123', email: 'jinchengyang@gwmail.gwu.edu')
             l.save()
             new LoginRole(login: l, role: Role.findByAuthority(Role.ADMIN)).save(flush: true)
         }
 
         if (!Login.findByUsername('akahata')) {
-            def l = new Login(username: 'akahata', password: 'gwu123', email: 'anc@email.com')
+            def l = new Login(username: 'akahata', password: 'gwu123', email: 'akahata11@gwmail.gwu.edu')
             l.save()
             new LoginRole(login: l, role: Role.findByAuthority(Role.USER)).save(flush: true)
         }
 
         if (!Login.findByUsername('jay')) {
-            def l = new Login(username: 'jay', password: 'gwu123', email: 'anc@email.com')
+            def l = new Login(username: 'jay', password: 'gwu123', email: 'somnusjay@gwmail.gwu.edu')
             l.save()
             new LoginRole(login: l, role: Role.findByAuthority(Role.USER)).save(flush: true)
         }
